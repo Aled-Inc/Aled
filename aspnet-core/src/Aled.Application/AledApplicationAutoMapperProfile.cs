411b7e5f389ddfc16ab2c60b60a3065d0a5a617a
Antonin Artaud <antonin.artaud.aa@gmail.com>
2024-03-24 18:15:39 +0100

    Add Dockerfile for Aled.AuthServer.csproj, Aled.Blazor.csproj, Aled.HttpApi.Host.csproj, and cleanup some file ( Program.cs, AledApplicationAutoMapperProfile.cs)

--- aspnet-core/src/Aled.Application/AledApplicationAutoMapperProfile.cs
+++ aspnet-core/src/Aled.Application/AledApplicationAutoMapperProfile.cs
@@ -4,4 +4,10 @@ namespace Aled;
 
 public class AledApplicationAutoMapperProfile : Profile
 {
+    public AledApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+    }
 }
