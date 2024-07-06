f40e94376d20609e4cb08e1957c56b1d050fbabb
krxtch <lorysjele@gmail.com>
2024-07-06 14:22:09 +0200

    finish login page design #7

--- react-native/src/screens/Login/LoginScreen.js
+++ react-native/src/screens/Login/LoginScreen.js
@@ -1,12 +1,11 @@
 import {useFormik} from 'formik';
 import i18n from 'i18n-js';
-import {Box, Button, Center, FormControl, Image, Input, Stack, WarningOutlineIcon} from 'native-base';
+import {Box, Button, Center, FormControl, Input, Stack, WarningOutlineIcon} from 'native-base';
 import PropTypes from 'prop-types';
 import React, {useRef, useState} from 'react';
-import {View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import {object, string} from 'yup';
 import {login} from '../../api/AccountAPI';
-import TenantBox from '../../components/TenantBox/TenantBox';
 import ValidationMessage from '../../components/ValidationMessage/ValidationMessage';
 import AppActions from '../../store/actions/AppActions';
 import LoadingActions from '../../store/actions/LoadingActions';
@@ -19,12 +18,7 @@ const ValidationSchema = object().shape({
 });
 
 function LoginScreen({startLoading, stopLoading, setToken, fetchAppConfig}) {
-    const [showTenantSelection, setShowTenantSelection] = useState(false);
     const passwordRef = useRef(null);
-
-    const toggleTenantSelection = () => {
-        setShowTenantSelection(!showTenantSelection);
-    };
 
     const submit = ({username, password}) => {
         startLoading({key: 'login'});
@@ -56,34 +50,16 @@ function LoginScreen({startLoading, stopLoading, setToken, fetchAppConfig}) {
 
     return (
         <Center flex={0.6} px="3">
-            <Box
-                w={{
-                    base: '100%',
-                }}
-                mb="50"
-                alignItems="center"
-            >
-                <Image
-                    alt="Image"
-                    source={require('../../../assets/logo.png')}
-                />
+            <Box style={styles.titleBox}>
+                <Text style={styles.appTitle}>Aled.</Text>
             </Box>
-
-            <TenantBox
-                showTenantSelection={showTenantSelection}
-                toggleTenantSelection={toggleTenantSelection}
-            />
-            <Box
-                w={{
-                    base: '100%',
-                }}
-                display={showTenantSelection ? 'none' : 'flex'}
-            >
+            <Box style={styles.formBox}>
+                <View style={{marginBottom: 20, alignItems: 'center'}}>
+                  <Text style={styles.title}>Log In</Text>
+                  <Text style={styles.subtitle}>Login to open your fridge</Text>
+                </View>
                 <FormControl isRequired my="2">
-                    <Stack mx="4">
-                        <FormControl.Label>
-                            {i18n.t('AbpAccount::UserNameOrEmailAddress')}
-                        </FormControl.Label>
+                    <Stack mx="12">
                         <Input
                             onChangeText={formik.handleChange('username')}
                             onBlur={formik.handleBlur('username')}
@@ -92,16 +68,17 @@ function LoginScreen({startLoading, stopLoading, setToken, fetchAppConfig}) {
                             autoCapitalize="none"
                             onSubmitEditing={() => passwordRef?.current?.focus()}
                             size="lg"
+                            placeholder='email'
+                            style={styles.input}
+                            variant={'rounded'}
+                            borderWidth={'0'}
                         />
                         <ValidationMessage>{formik.errors.username}</ValidationMessage>
                     </Stack>
                 </FormControl>
 
                 <FormControl isRequired my="2">
-                    <Stack mx="4">
-                        <FormControl.Label>
-                            {i18n.t('AbpAccount::Password')}
-                        </FormControl.Label>
+                    <Stack mx="12">
                         <Input
                             type="password"
                             onChangeText={formik.handleChange('password')}
@@ -110,7 +87,12 @@ function LoginScreen({startLoading, stopLoading, setToken, fetchAppConfig}) {
                             ref={passwordRef}
                             autoCapitalize="none"
                             size="lg"
+                            placeholder='password'
+                            style={styles.input}
+                            variant={'rounded'}
+                            borderWidth={'0'}
                         />
+                        <Text style={styles.forgotPassword}>I forgot my password</Text>
                         <FormControl.ErrorMessage
                             leftIcon={<WarningOutlineIcon size="xs"/>}
                         >
@@ -120,9 +102,12 @@ function LoginScreen({startLoading, stopLoading, setToken, fetchAppConfig}) {
                 </FormControl>
 
                 <View style={{marginTop: 20, alignItems: 'center'}}>
-                    <Button onPress={formik.handleSubmit} width="30%" size="lg">
-                        {i18n.t('AbpAccount::Login')}
+                    <Button onPress={formik.handleSubmit} width="40%" size="lg" style={styles.button}>
+                        {/* {i18n.t('AbpAccount::Login')} */}
+                        <Text style={styles.button.text}>Login</Text>
                     </Button>
+                    <Text style={styles.dontHaveAccount}>Maybe you don't have any account ?</Text>
+                    <Text style={styles.registerHere}>Register here</Text>
                 </View>
             </Box>
         </Center>
@@ -135,6 +120,68 @@ LoginScreen.propTypes = {
     setToken: PropTypes.func.isRequired,
     fetchAppConfig: PropTypes.func.isRequired,
 };
+
+const styles = StyleSheet.create({
+  titleBox: {
+    width: '100%',
+    marginTop: '55%',
+    alignItems: 'center'
+  },
+  formBox: {
+    width: '100%',
+    marginTop: '35%',
+  },
+  button: {
+    backgroundColor: '#FFC700',
+    borderRadius: 25,
+    height: 45,
+    marginTop: 100,
+    marginBottom: 15,
+    text: {
+      fontFamily: 'Inter-Light',
+      fontSize: 14,
+      color: '#000'
+    }
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 15,
+    fontSize: 14,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontFamily: 'Inter-Medium',
+    fontSize: 40
+  },
+  subtitle: {
+    fontFamily: 'Inter-Light',
+    fontSize: 15,
+    color: '#808080',
+    marginTop: 5,
+    marginBottom: 20
+  },
+  appTitle: {
+    fontFamily: 'Inter-Light',
+    fontSize: 16,
+    color: '#808080'
+  },
+  forgotPassword: {
+    fontFamily: 'Inter-Light',
+    fontSize: 12,
+    paddingTop: 5,
+    paddingLeft: 10
+  },
+  dontHaveAccount: {
+    fontFamily: 'Inter-Light',
+    fontSize: 14,
+  },
+  registerHere: {
+    fontFamily: 'Inter-Light',
+    fontSize: 14,
+    color: '#808080',
+    textDecorationLine: 'underline'
+  }
+});
 
 export default connectToRedux({
     component: LoginScreen,
