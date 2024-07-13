83cc07336bc7d25cf1d701195793560626695dd6
krxtch <lorysjele@gmail.com>
2024-07-13 16:38:35 +0200

    add ngrok docker conf - improve env conf (back/front)

--- aspnet-core/src/Aled.HttpApi.Client/updateProxy.sh
+++ aspnet-core/src/Aled.HttpApi.Client/updateProxy.sh
@@ -1,1 +1,6 @@
-abp generate-proxy -t csharp -u https://192.0.0.2:44305
+# You should run this with Git Bash to avoid errors 
+
+# shellcheck disable=SC2046
+export $(grep -v '^#' ../.env | xargs)
+
+abp generate-proxy -t csharp -u "$REMOTE_SERVICE_URL"
